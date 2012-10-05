ef938046c9adb3b634b24e25ae213906c95cd97b
Philipe Fatio <philipe.fatio@gmail.com>
2012-10-05 09:35:49 +0200

    Initial Commit

---
+++ PHFArrayComparator/PHFAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  PHFAppDelegate.h
+//  PHFArrayComparator
+//
+//  Created by Philipe on 05.10.12.
+//  Copyright (c) 2012 Philipe Fatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PHFViewController;
+
+@interface PHFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) PHFViewController *viewController;
+
+@end
---
+++ PHFArrayComparator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PHFArrayComparator
+//
+//  Created by Philipe on 05.10.12.
+//  Copyright (c) 2012 Philipe Fatio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PHFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PHFAppDelegate class]));
+    }
+}
